c7805684d580b16d993e79947bbb6f9550a030aa
garrulus2003 <54739772+garrulus2003@users.noreply.github.com>
2021-04-11 23:52:06 +0300

    Добавлена возможность переименовать гуся. Сюда же переехало определение шрифта: ему и правда нечего делать в sizes

--- goose.py
+++ goose.py
@@ -1,9 +1,11 @@
 import time
-
-import self as self
-
-from sizes import *
+from tkinter import *
+import tkinter
 from text_graphics import *
+
+pygame.init()
+pygame.mixer.init()
+f = FONT = pygame.font.Font(None, 24)
 
 
 class Characteristics:
@@ -20,6 +22,22 @@ class Characteristics:
             self.lvl += 1.1
             self.lvl = min(self.lvl, 10.1)
             self.time_passed = time.time()
+
+
+def draw_goose_renaming():
+    root = Tk()
+    root.geometry(str(RENAME_WIDTH) + "x" + str(RENAME_HEIGHT))
+    root["bg"] = '#ffb259'  # orange colour
+    root.title("Введите новое имя гуся")
+    new_name = StringVar()
+    enter_name = Entry(root, bg="#ffffff", bd=2, textvariable=new_name, width=15, font=("Arial Bold", 20),
+                       justify='center')
+    rename = tkinter.Button(root, text="Переименовать", height=1, width=15, font=("Arial Bold", 14),
+                            command=root.destroy, justify='center')
+    enter_name.place(x=150, y=30, anchor="c")
+    rename.place(x=60, y=60)
+    root.mainloop()
+    return new_name.get()
 
 
 class Goose:
@@ -45,6 +63,11 @@ class Goose:
         self.is_dead()
 
 
+def rename():
+    text = draw_goose_renaming()
+    pygame.display.set_caption("Гусь " + text)
+
+
 class GooseButtons:
     def __init__(self, goose_, screen_):
         self.goose = goose_
@@ -60,6 +83,11 @@ class GooseButtons:
             lbl = Label(self.goose.parameters[i].upgrade_name, FONT, rectangles[i], DARK_GREY,
                         GREY, BUTTON_TITLE_HORIZONTAL, BUTTON_TITLE_VERTICAL, self.screen)
             self.buttons.append(Button(lbl, self.goose.parameters[i].upgrade, BUTTON_BORDER))
+        rename_goose_rect = pygame.Rect((right_column, UPPER_INDENT, BUTTON_WIDTH, BUTTON_HEIGHT / 2))
+        rename_goose_lbl = Label("Переименовать", FONT, rename_goose_rect, DARK_GREY, GREY,
+                                 BUTTON_TITLE_HORIZONTAL / 2, BUTTON_TITLE_VERTICAL / 3, self.screen)
+        rename_goose = Button(rename_goose_lbl, rename, BUTTON_BORDER)
+        self.buttons.append(rename_goose)
 
     def show(self):
         for button in self.buttons:
